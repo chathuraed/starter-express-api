cec57c0348dec6086f3a62302c549661e910c61a
chathuraed <chathura.dev93@gmail.com>
2023-11-08 02:19:39 +0530

    import fixed

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,9 +1,9 @@
 // routes/auth.js
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-import adminController from "../controllers/adminController";
-import { verifyToken } from "../middlewares/authMiddleware";
+const adminController = require("../controllers/adminController");
+const { verifyToken } = require("../middlewares/authMiddleware");
 
 router.get("/users", verifyToken(["admin"]), adminController.listUsers);
 router.get("/user/:id", verifyToken(["admin"]), adminController.getUser);
