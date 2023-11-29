2f8132b6dad0b1c9982e28838ef519bf1c8acf29
chathuraed <chathura.dev93@gmail.com>
2023-11-30 03:50:50 +0530

    create or update schedule

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -15,6 +15,7 @@ const scheduleController = {
     try {
       const {
         routeId,
+        scheduleId,
         origin,
         destination,
         start_time,
@@ -35,21 +36,40 @@ const scheduleController = {
         return res.status(404).json({ error: "Route not found" });
       }
 
-      const newSchedule = new Schedule({
-        route: route._id,
-        origin,
-        destination,
-        start_time,
-        end_time,
-        available_at,
-      });
+      if (scheduleId) {
+        // If scheduleId is provided, update the existing schedule
+        const existingSchedule = await Schedule.findById(scheduleId).exec();
 
-      const savedSchedule = await newSchedule.save();
+        if (!existingSchedule) {
+          return res.status(404).json({ error: "Schedule not found" });
+        }
 
-      route.schedules.push(savedSchedule._id);
-      await route.save();
+        existingSchedule.origin = origin;
+        existingSchedule.destination = destination;
+        existingSchedule.start_time = start_time;
+        existingSchedule.end_time = end_time;
+        existingSchedule.available_at = available_at;
 
-      return res.status(201).json(savedSchedule);
+        const updatedSchedule = await existingSchedule.save();
+        return res.status(200).json(updatedSchedule);
+      } else {
+        // Create a new schedule
+        const newSchedule = new Schedule({
+          route: route._id,
+          origin,
+          destination,
+          start_time,
+          end_time,
+          available_at,
+        });
+
+        const savedSchedule = await newSchedule.save();
+
+        route.schedules.push(savedSchedule._id);
+        await route.save();
+
+        return res.status(201).json(savedSchedule);
+      }
     } catch (err) {
       return res.status(500).json({ error: "Internal Server Error" });
     }
